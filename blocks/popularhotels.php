c5d11f3adb59d4d9c1f06599c6af0bb662d5acee
Vladimir Samojlov <v-moi@ya.ru>
2018-12-12 01:39:02 +0200

    edit hotel-item

--- blocks/popularhotels.php
+++ blocks/popularhotels.php
@@ -25,24 +25,23 @@
 			  ) );
 			  if ($custom_query->have_posts()) : while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
 					<div class="col-md-3">
-						<a href="<?php the_permalink(); ?>">
-							<div class="hotel-item">
-								<div class="hotel-item__img mb-4">
-									<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
-									<div class="hotel-item__img__city">
-										<?php $post_id = rwmb_meta( 'meta-hotelcity' ); ?>
-
-												<div class="hotel-item__img__city__link">
-													<a href="<?php echo get_permalink($post_id) ?>"><img src="<?php bloginfo('template_url'); ?>/img/pin.svg" alt="" class="hotel-item__icon mr-2"><span><?php echo get_the_title( $post_id ); ?><span></a>	
-												</div>
-										
+						<div class="hotel-item">
+							<div class="hotel-item__img mb-4">
+								<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+								<div class="hotel-item__img__city">
+									<?php $post_id = rwmb_meta( 'meta-hotelcity' ); ?>
+									<div class="hotel-item__img__city__link">
+										<a href="<?php echo get_permalink($post_id) ?>"><img src="<?php bloginfo('template_url'); ?>/img/pin.svg" alt="" class="hotel-item__icon mr-2"><span><?php echo get_the_title( $post_id ); ?><span></a>	
 									</div>
 								</div>
-								<div class="hotel-item__title">
-									<?php the_title(); ?>
-								</div>
 							</div>
-						</a>
+							<div class="hotel-item__title">
+								<a href="<?php the_permalink(); ?>" class="pb-3"><?php the_title(); ?></a>
+							</div>
+							<div class="hotel-item__rating pt-3">
+								Рейтинг: <?php echo rwmb_meta( 'meta-hotel-mainrating' ); ?> из 100
+							</div>
+						</div>
 					</div>
 				<?php endwhile; endif; ?>
 			</div>
@@ -67,26 +66,27 @@
 						  ) );
 						  if ($custom_query->have_posts()) : while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
 								<div class="swiper-slide">
-			          	<a href="<?php the_permalink(); ?>">
-										<div class="hotel-item">
-											<div class="hotel-item__img mb-4">
-												<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
-												<div class="hotel-item__img__city">
-													<?php 
-														$terms = get_the_terms( $post->ID , 'tx_cities' );
-														foreach ( $terms as $term ): ?>
-															<?php $term_link = get_term_link($term); ?>
-															<div class="hotel-item__img__city__link">
-																<a href="<?php echo $term_link; ?>"><img src="<?php bloginfo('template_url'); ?>/img/pin.svg" alt="" class="hotel-item__icon mr-2"><span><?php echo $term->name; ?><span></a>	
-															</div>
-													<?php endforeach; ?>
-												</div>
-											</div>
-											<div class="hotel-item__title">
-												<?php the_title(); ?>
+									<div class="hotel-item">
+										<div class="hotel-item__img mb-4">
+											<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+											<div class="hotel-item__img__city">
+												<?php 
+													$terms = get_the_terms( $post->ID , 'tx_cities' );
+													foreach ( $terms as $term ): ?>
+														<?php $term_link = get_term_link($term); ?>
+														<div class="hotel-item__img__city__link">
+															<a href="<?php echo $term_link; ?>"><img src="<?php bloginfo('template_url'); ?>/img/pin.svg" alt="" class="hotel-item__icon mr-2"><span><?php echo $term->name; ?><span></a>	
+														</div>
+												<?php endforeach; ?>
 											</div>
 										</div>
-									</a>
+										<div class="hotel-item__title">
+											<a href="<?php the_permalink(); ?>" class="pb-3"><?php the_title(); ?></a>
+										</div>
+										<div class="hotel-item__rating pt-3">
+											Рейтинг: <?php echo rwmb_meta( 'meta-hotel-mainrating' ); ?> из 100
+										</div>
+									</div>
 			          </div>
 		          <?php endwhile; endif; ?>
 				    </div>
