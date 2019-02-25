eef7fa827e12bc3897bab5dc9878ab9c0bbbccd9
Vladimir Samojlov <v-moi@ya.ru>
2019-02-26 00:22:04 +0200

    edit swiper hotel card

--- blocks/hotel-card.php
+++ blocks/hotel-card.php
@@ -26,8 +26,12 @@
 					<div class="hotel-item__img-leftbg"></div>
 					<div class="hotel-item__img-rightbg"></div>	
 				</div>
-				<div class="swiper-button-next swiper-hotelcard-button-next"></div>
-		    <div class="swiper-button-prev swiper-hotelcard-button-prev"></div>
+		    <div class="swiper-hotelcard-button-prev">
+		    	<svg viewBox="0 0 18 18" role="img" aria-label="Предыдущее фото" focusable="false" style="height: 24px; width: 24px; display: block; fill: rgb(255, 255, 255);"><path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fill-rule="evenodd"></path></svg>
+		    </div>
+		    <div class="swiper-hotelcard-button-next">
+					<svg viewBox="0 0 18 18" role="img" aria-label="Следующее фото" focusable="false" style="height: 24px; width: 24px; display: block; fill: rgb(255, 255, 255);"><path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fill-rule="evenodd"></path></svg>
+				</div>
 			</div>
 			<!-- <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=""> -->
 			<div class="hotel-item__img__city">
