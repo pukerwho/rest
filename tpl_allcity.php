c2cfb55b57114580253f41f5cae075fb17de35be
Vladimir Samojlov <v-moi@ya.ru>
2019-03-06 01:34:22 +0200

    edit tpl allcity

--- tpl_allcity.php
+++ tpl_allcity.php
@@ -7,19 +7,25 @@ Template Name: Все города
 <?php get_header(); ?>
 
 <div class="maincards">
-	<div class="maincards__grid">
-		<?php $citylists = get_terms( array( 'taxonomy' => 'citylist', 'parent' => 0 ) );
-		foreach ( $citylists as $citylist ): ?>
-		<div class="maincards__item">
-			<a href="<?php echo get_term_link($citylist); ?>">
-				<div class="maincards__item__card" style="background: url('<?php echo carbon_get_term_meta($citylist->term_id, 'crb_citylist_img' ); ?>')">
-					<div class="maincards__item__card__title">
-						<?php echo $citylist->name ?>
+	<div class="container ">
+		<div class="row justify-content-center">
+			<div class="col-md-12">
+				<div class="maincards__grid">
+					<?php $citylists = get_terms( array( 'taxonomy' => 'citylist', 'parent' => 0 ) );
+					foreach ( $citylists as $citylist ): ?>
+					<div class="maincards__item">
+						<a href="<?php echo get_term_link($citylist); ?>">
+							<div class="maincards__item__card" style="background: url('<?php echo carbon_get_term_meta($citylist->term_id, 'crb_citylist_img' ); ?>')">
+								<div class="maincards__item__card__title">
+									<?php echo $citylist->name ?>
+								</div>
+							</div>
+						</a>
 					</div>
+					<?php endforeach; ?>
 				</div>
-			</a>
+			</div>
 		</div>
-		<?php endforeach; ?>
 	</div>
 </div>
 
