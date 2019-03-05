f3075a2de18b54efd43a557500f76cc0610e09d6
Vladimir Samojlov <v-moi@ya.ru>
2019-03-06 01:19:58 +0200

    edit bug in name class

--- blocks/nomers/standart.php
+++ blocks/nomers/standart.php
@@ -52,7 +52,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="nober-modal__content">
+	<div class="nomer-modal__content">
 		<div class="container py-5">
 			<div class="row">
 				<div class="col-md-12">
