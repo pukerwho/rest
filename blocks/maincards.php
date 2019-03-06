ab2ad1cc17b27d7c86bb56855235689222a90a36
Vladimir Samojlov <v-moi@ya.ru>
2019-03-06 13:41:09 +0200

    edit allcity

--- blocks/maincards.php
+++ blocks/maincards.php
@@ -5,7 +5,7 @@
 				<h2 class="mb-5">Где отдыхать летом в Украине?</h2>
 				<!-- MAINCARDS PC VERSION -->
 				<div class="maincards__grid pc-show">
-					<?php $citylists = get_terms( array( 'taxonomy' => 'citylist', 'parent' => 0, 'hide_empty' => false ) );
+					<?php $citylists = get_terms( array( 'taxonomy' => 'citylist', 'parent' => 0, 'hide_empty' => false, 'posts_per_page' => 5 ) );
 					foreach ( $citylists as $citylist ): ?>
 					<div class="maincards__item">
 						<a href="<?php echo get_term_link($citylist); ?>">
