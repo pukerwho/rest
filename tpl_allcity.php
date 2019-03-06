7a4b29d51febfe3cfede05beec8d8209bc7c5c8c
Vladimir Samojlov <v-moi@ya.ru>
2019-03-06 13:39:03 +0200

    edit allcity

--- tpl_allcity.php
+++ tpl_allcity.php
@@ -8,10 +8,15 @@ Template Name: Все города
 
 <div class="maincards">
 	<div class="container ">
+		<div class="row mb-5">
+			<div class="col-md-12">
+				<h1>Отдых в Украине. Куда поехать?</h1>
+			</div>
+		</div>
 		<div class="row justify-content-center">
 			<div class="col-md-12">
 				<div class="maincards__grid">
-					<?php $citylists = get_terms( array( 'taxonomy' => 'citylist', 'parent' => 0 ) );
+					<?php $citylists = get_terms( array( 'taxonomy' => 'citylist', 'parent' => 0, 'hide_empty' => false ) );
 					foreach ( $citylists as $citylist ): ?>
 					<div class="maincards__item">
 						<a href="<?php echo get_term_link($citylist); ?>">
