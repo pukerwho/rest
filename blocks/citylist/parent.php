40e22f8d2ac974b04c51df923def09e9b5e65ea7
Vladimir Samojlov <v-moi@ya.ru>
2019-03-30 01:26:41 +0200

    some mini global update

--- blocks/citylist/parent.php
+++ blocks/citylist/parent.php
@@ -13,6 +13,11 @@
 					  <li class="nav-item">
 					    <a class="nav-link" id="way-tab" data-toggle="tab" href="#way" role="tab" aria-controls="way" aria-selected="false">Как добраться</a>
 					  </li>
+					  <?php if(carbon_get_term_meta(get_queried_object_id(), 'crb_citylist_video')): ?>
+					  <li class="nav-item">
+					    <a class="nav-link" id="video-tab" data-toggle="tab" href="#video" role="tab" aria-controls="video" aria-selected="false">Видео</a>
+					  </li>
+					  <?php endif ?>
 					  <li class="nav-item">
 					    <a class="nav-link" id="cityreviews-tab" data-toggle="tab" href="#cityreviews" role="tab" aria-controls="cityreviews" aria-selected="false">Обсуждение</a>
 					  </li>
@@ -34,6 +39,12 @@
 				  <div class="tab-pane tab-single-hotel fade" id="way" role="tabpanel" aria-labelledby="way-tab">
 				  	<h2>Еще нет ничего</h2>
 				  </div>
+				  <?php if(carbon_get_term_meta(get_queried_object_id(), 'crb_citylist_video')): ?>
+				  <div class="tab-pane tab-single-hotel fade" id="video" role="tabpanel" aria-labelledby="video-tab">
+				  	<h3 class="mb-4">Наслаждайтесь видео!</h3>
+				  	<div class="youtube-player" data-id="<?php echo carbon_get_term_meta(get_queried_object_id(), 'crb_citylist_video') ?>"></div>
+				  </div>
+				  <?php endif ?>
 				  <div class="tab-pane tab-single-hotel fade" id="cityreviews" role="tabpanel" aria-labelledby="cityreviews-tab">
 				  	<?php global $withcomments;
 						$withcomments = true;
--- inc/custom-fields/citylist-meta.php
+++ inc/custom-fields/citylist-meta.php
@@ -13,6 +13,7 @@ function crb_attach_theme_options() {
       Field::make( 'text', 'crb_citylist_description', 'Подзаголовок' ),
       Field::make( 'image', 'crb_citylist_icon', 'Иконка' )->set_value_type( 'url'),
       Field::make( 'rich_text', 'crb_citylist_rich_text', 'Текст' ),
+      Field::make( 'text', 'crb_citylist_video', 'Видео (id YouTube)' )
   ) );
 }
 
