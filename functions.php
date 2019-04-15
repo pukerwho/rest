0e9c7bc8fca583d80fab5fb8fdf5ee969c22e527
Vladimir Samojlov <v-moi@ya.ru>
2019-04-15 10:36:39 +0300

    edit blogs

--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ function customThemeSupport() {
     add_theme_support( 'post-thumbnails' );
     // let wordpress manage the title
     add_theme_support( 'title-tag' );
+    add_theme_support( 'caption' );
+    add_theme_support( 'gallery' );
     //add_theme_support( 'custom-background', $args );
     //add_theme_support( 'custom-header', $args );
     // Automatic feed links compatibility
