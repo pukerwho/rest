0443acf3df8fd50979b277d77f29babcc7dd3485
Vladimir Samojlov <v-moi@ya.ru>
2019-10-19 23:58:26 +0300

    edit main page padding

--- tpl_main.php
+++ tpl_main.php
@@ -10,9 +10,6 @@ Template Name: Главная страница
 <?php /* get_template_part( 'blocks/who', 'default' );  */ ?>
 <?php get_template_part( 'blocks/maincards', 'default' ); ?>
 <div class="pt-5">
-	<?php get_template_part( 'blocks/newhotels', 'default' ); ?>	
-</div>
-<div class="pt-5">
 	<?php get_template_part( 'blocks/b_rest', 'default' ); ?>	
 </div>
 <div class="pt-5">
