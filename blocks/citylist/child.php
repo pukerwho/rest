a27839e1778b4a0365884a44a040cd57ea3f938b
Vladimir Samojlov <v-moi@ya.ru>
2019-10-29 17:05:25 +0200

    test comments

--- blocks/citylist/child.php
+++ blocks/citylist/child.php
@@ -154,8 +154,8 @@
 							),
 						) );
 						if ($custom_query_post_comment->have_posts()) : while ($custom_query_post_comment->have_posts()) : $custom_query_post_comment->the_post(); ?>
-					  	<?php global $comment;
-							$comment = true;
+					  	<?php global $withcomments;
+							$withcomments = true;
 							comments_template(); ?> 
 						<?php endwhile; endif; wp_reset_postdata(); ?>
 				  </div>
