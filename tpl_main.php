2446377e5e39f8f4b48cb7be2b17f4f94f606fb7
Vladimir Samojlov <v-moi@ya.ru>
2019-11-15 16:53:49 +0200

    edit text on main page

--- tpl_main.php
+++ tpl_main.php
@@ -40,5 +40,18 @@ Template Name: Главная страница
 	<?php get_template_part( 'blocks/animalshotels', 'default' ); ?>	
 </div>
 
+<div class="container pt-5">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="b_rest__text citylist__text">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; else: ?>
+					<p><?php _e('Ничего не найдено'); ?></p>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
