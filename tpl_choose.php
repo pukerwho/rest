413eced8228f3ebdca0609f4051fa6c784a69936
Vladimir Samojlov <v-moi@ya.ru>
2020-01-16 17:35:57 +0200

    add link to forms

--- tpl_choose.php
+++ tpl_choose.php
@@ -44,7 +44,7 @@ Template Name: Выбрать тариф
 					<li class="mb-3"><?php _e( 'До 5 фотографий', 'restx' ); ?></li>
 					<li class="mb-3">500-1000 <?php _e( 'просмотров за месяц', 'restx' ); ?></li>
 				</div>
-				<a href="#" data-toggle="modal" data-target="#budgetModal" class="text-dark">
+				<a href="<?php echo home_url(); ?>/tarif-base/" class="text-dark">
 					<div class="btn bg-pastel-green">
 						<?php _e( 'Выбрать', 'restx' ); ?>
 					</div>
@@ -71,7 +71,7 @@ Template Name: Выбрать тариф
 					<li class="mb-3"><?php _e( 'Техническая поддержка', 'restx' ); ?></li>
 					<li class="mb-3">2500-5000 <?php _e( 'просмотров за месяц', 'restx' ); ?></li>
 				</div>
-				<a href="#" data-toggle="modal" data-target="#standartModal" class="text-dark">
+				<a href="<?php echo home_url(); ?>/tarif-standart/" class="text-dark">
 					<div class="btn bg-pastel-red">
 						<?php _e( 'Выбрать', 'restx' ); ?>
 					</div>
@@ -101,7 +101,7 @@ Template Name: Выбрать тариф
 					<li class="mb-3"><?php _e( 'Упоминание в e-mail рассылке', 'restx' ); ?></li>
 					<li class="mb-3">7000-10000 <?php _e( 'просмотров за месяц', 'restx' ); ?></li>
 				</div>
-				<a href="#" data-toggle="modal" data-target="#premiumModal" class="text-dark">
+				<a href="<?php echo home_url(); ?>/tarif-premium/" class="text-dark">
 					<div class="btn bg-pastel-blue">
 						<?php _e( 'Выбрать', 'restx' ); ?>
 					</div>
--- tpl_partner.php
+++ tpl_partner.php
@@ -178,7 +178,7 @@ Template Name: Партнерство
 						<li class="mb-3"><?php _e( 'До 5 фотографий', 'restx' ); ?></li>
 						<li class="mb-3">500-1000 <?php _e( 'просмотров за месяц', 'restx' ); ?></li>
 					</div>
-					<a href="#" data-toggle="modal" data-target="#budgetModal" class="text-dark">
+					<a href="<?php echo home_url(); ?>/tarif-base/" class="text-dark">
 						<div class="btn bg-pastel-green">
 							<?php _e( 'Выбрать', 'restx' ); ?>
 						</div>
@@ -205,7 +205,7 @@ Template Name: Партнерство
 						<li class="mb-3"><?php _e( 'Техническая поддержка', 'restx' ); ?></li>
 						<li class="mb-3">2500-5000 <?php _e( 'просмотров за месяц', 'restx' ); ?></li>
 					</div>
-					<a href="#" data-toggle="modal" data-target="#standartModal" class="text-dark">
+					<a href="<?php echo home_url(); ?>/tarif-standart/" class="text-dark">
 						<div class="btn bg-pastel-red">
 							<?php _e( 'Выбрать', 'restx' ); ?>
 						</div>
@@ -235,7 +235,7 @@ Template Name: Партнерство
 						<li class="mb-3"><?php _e( 'Упоминание в e-mail рассылке', 'restx' ); ?></li>
 						<li class="mb-3">7000-10000 <?php _e( 'просмотров за месяц', 'restx' ); ?></li>
 					</div>
-					<a href="#" data-toggle="modal" data-target="#premiumModal" class="text-dark">
+					<a href="<?php echo home_url(); ?>/tarif-premium/" class="text-dark">
 						<div class="btn bg-pastel-blue">
 							<?php _e( 'Выбрать', 'restx' ); ?>
 						</div>
