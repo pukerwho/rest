ff0caf95fff0bae3bc77a52e0d2be584f60756d2
Vladimir Samojlov <v-moi@ya.ru>
2020-02-10 13:45:58 +0200

    test

--- blocks/citylist/parent.php
+++ blocks/citylist/parent.php
@@ -138,6 +138,7 @@
 					),
 				) );
 				if ($custom_query_post_comment->have_posts()) : while ($custom_query_post_comment->have_posts()) : $custom_query_post_comment->the_post(); ?>
+					<?php the_title(); ?>
 			  	<?php echo do_shortcode('[anycomment]'); ?>
 				<?php endwhile; endif; wp_reset_postdata(); ?>
 			</div>
