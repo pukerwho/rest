ad8f3a38cc21d0f319d3a265f22d8cd29b01aee1
Vladimir Samojlov <v-moi@ya.ru>
2020-05-24 19:03:00 +0300

    edit

--- blocks/p_main-blog.php
+++ blocks/p_main-blog.php
@@ -1,3 +1,13 @@
+<?php
+	if (get_locale() == 'ru_RU') {
+		$blog_other_slug = 'common';
+		$blog_how_term_slug = 'kak-dobratsja';	
+	} else {
+		$blog_other_slug = 'articles-uk';
+		$blog_how_term_slug = 'jak-distatis';
+	}
+?>
+
 <div class="container-fluid cf_px">
 	<div class="row">
 		<div class="col-md-12">
@@ -13,31 +23,16 @@
 	</div>
 	<div class="row">
 		<?php 
-			if (get_locale() == 'ru_RU') {
-				$blog_other_slug = 'common';
-				$blog_how_term_slug = 'kak-dobratsja';	
-			} else {
-				$blog_other_slug = 'articles-uk';
-				$blog_how_term_slug = 'jak-distatis';
-			}
-			
-
 			$blog_how_to_query = new WP_Query( array( 
 				'post_type' => 'blogs', 
 				'posts_per_page' => 2,
 				'orderby' => 'rand',
-				'tax_query' => array(
-					array(
-						'taxonomy' => 'blog-categories',
-						'field' => 'slug',
-						'terms' => $blog_how_term_slug
-					)
-				)
+				
 			) );
 		if ($blog_how_to_query->have_posts()) : while ($blog_how_to_query->have_posts()) : $blog_how_to_query->the_post(); ?>
 		<div class="col-md-4 mb-4">
 			<div class="home_blog main">
-				<a href="<?php get_the_permalink(); ?>">
+				<a href="<?php echo get_the_permalink(); ?>">
 					<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'large'); ?>" alt="" class="home_blog_img mb-4">
 				</a>
 				<div class="home_blog_cat mb-2">
@@ -48,7 +43,7 @@
 					<?php endforeach; ?>
 				</div>
 				<div class="home_blog_title">
-					<a href="<?php get_the_permalink(); ?>">
+					<a href="<?php echo get_the_permalink(); ?>">
 						<?php the_title(); ?>
 					</a>
 				</div>
@@ -73,7 +68,9 @@
 				<div class="home_blog second mb-4">
 					<div class="row d-flex flex-row ">
 						<div class="col-6">
-							<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="" class="home_blog_img mb-4">
+							<a href="<?php echo get_the_permalink(); ?>">
+								<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="" class="home_blog_img mb-4">
+							</a>
 						</div>
 						<div class="col-6">
 							<div class="home_blog_cat mr-2">
@@ -84,7 +81,7 @@
 								<?php endforeach; ?>
 							</div>
 							<div class="home_blog_title">
-								<a href="<?php get_the_permalink(); ?>">
+								<a href="<?php echo get_the_permalink(); ?>">
 									<?php the_title(); ?>
 								</a>
 							</div>
