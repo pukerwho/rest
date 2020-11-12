59115ea621921c7c94588dce707b171d66d2489a
Vladimir Samojlov <v-moi@ya.ru>
2020-11-12 14:25:44 +0200

    edit

--- blocks/home/homecities.php
+++ blocks/home/homecities.php
@@ -46,11 +46,17 @@
 								'parent' => 0, 
 								'hide_empty' => false,
 								'meta_query' => array(
+									'relation' => 'AND',
 						      array(
 										'key'       => '_crb_citylist_iscurort',
 										'value'     => 'yes',
 										'compare'   => '='
 						      ),
+						      array(
+										'key'       => '_crb_citylist_showmain',
+										'value'     => 'yes',
+										'compare'   => '='
+						      )
 						    )
 							));
 							shuffle( $maincitylists );
